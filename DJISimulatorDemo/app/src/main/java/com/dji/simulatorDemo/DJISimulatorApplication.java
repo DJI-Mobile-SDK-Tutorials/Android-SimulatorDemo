437868ec757522897b35162b7599cd8f51dcd44c
Oliver Ou <oliver.ou@dji.com>
2018-01-10 14:27:10 +0800

    update demo to Android SDK 4.4.1.

--- DJISimulatorDemo/app/src/main/java/com/dji/simulatorDemo/DJISimulatorApplication.java
+++ DJISimulatorDemo/app/src/main/java/com/dji/simulatorDemo/DJISimulatorApplication.java
@@ -1,6 +1,7 @@
 package com.dji.simulatorDemo;
 
 import android.app.Application;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Handler;
@@ -19,12 +20,27 @@ import dji.sdk.sdkmanager.DJISDKManager;
 public class DJISimulatorApplication extends Application {
 
     private static final String TAG = DJISimulatorApplication.class.getName();
-
     public static final String FLAG_CONNECTION_CHANGE = "com_dji_simulatorDemo_connection_change";
-
     private static BaseProduct mProduct;
-
     private Handler mHandler;
+    private DJISDKManager.SDKManagerCallback mDJISDKManagerCallback;
+    private BaseProduct.BaseProductListener mDJIBaseProductListener;
+    private BaseComponent.ComponentListener mDJIComponentListener;
+
+    private Application instance;
+
+    public void setContext(Application application) {
+        instance = application;
+    }
+
+    @Override
+    public Context getApplicationContext() {
+        return instance;
+    }
+
+    public DJISimulatorApplication() {
+
+    }
 
     /**
      * Gets instance of the specific product connected after the
@@ -50,70 +66,16 @@ public class DJISimulatorApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-
         mHandler = new Handler(Looper.getMainLooper());
+        mDJIComponentListener = new BaseComponent.ComponentListener() {
 
-        //Check the permissions before registering the application for android system 6.0 above.
-        int permissionCheck = ContextCompat.checkSelfPermission(this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE);
-        int permissionCheck2 = ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_PHONE_STATE);
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M || (permissionCheck == 0 && permissionCheck2 == 0)) {
-
-            //This is used to start SDK services and initiate SDK.
-            DJISDKManager.getInstance().registerApp(this, mDJISDKManagerCallback);
-        } else {
-            Toast.makeText(getApplicationContext(), "Please check if the permission is granted.", Toast.LENGTH_LONG).show();
-        }
-
-    }
-
-    private DJISDKManager.SDKManagerCallback mDJISDKManagerCallback = new DJISDKManager.SDKManagerCallback() {
-
-        @Override
-        public void onRegister(DJIError error) {
-
-            Handler handler = new Handler(Looper.getMainLooper());
-
-            if(error == DJISDKError.REGISTRATION_SUCCESS) {
-
-                handler.post(new Runnable() {
-
-                    @Override
-                    public void run() {
-                        Toast.makeText(getApplicationContext(),
-                                R.string.success,
-                                Toast.LENGTH_LONG).show();
-                    }
-                });
-
-                DJISDKManager.getInstance().startConnectionToProduct();
-            } else {
-                handler.post(new Runnable() {
-
-                    @Override
-                    public void run() {
-                        Toast.makeText(getApplicationContext(),
-                                R.string.sdk_registration_message,
-                                Toast.LENGTH_LONG).show();
-                    }
-                });
-
-            }
-            Log.v(TAG, error.getDescription());
-        }
-
-        @Override
-        public void onProductChange(BaseProduct oldProduct, BaseProduct newProduct) {
-
-            Log.v(TAG, String.format("onProductChanged oldProduct:%s, newProduct:%s", oldProduct, newProduct));
-            mProduct = newProduct;
-            if(mProduct != null) {
-                mProduct.setBaseProductListener(mDJIBaseProductListener);
+            @Override
+            public void onConnectivityChange(boolean isConnected) {
+                notifyStatusChange();
             }
 
-            notifyStatusChange();
-        }
-
-        private BaseProduct.BaseProductListener mDJIBaseProductListener = new BaseProduct.BaseProductListener() {
+        };
+        mDJIBaseProductListener = new BaseProduct.BaseProductListener() {
 
             @Override
             public void onComponentChange(BaseProduct.ComponentKey key, BaseComponent oldComponent, BaseComponent newComponent) {
@@ -121,43 +83,91 @@ public class DJISimulatorApplication extends Application {
                 if(newComponent != null) {
                     newComponent.setComponentListener(mDJIComponentListener);
                 }
-                Log.v(TAG, String.format("onComponentChange key:%s, oldComponent:%s, newComponent:%s", key, oldComponent, newComponent));
-
                 notifyStatusChange();
             }
 
             @Override
             public void onConnectivityChange(boolean isConnected) {
 
-                Log.v(TAG, "onConnectivityChange: " + isConnected);
-
                 notifyStatusChange();
             }
 
         };
 
-        private BaseComponent.ComponentListener mDJIComponentListener = new BaseComponent.ComponentListener() {
+        /**
+         * When starting SDK services, an instance of interface DJISDKManager.DJISDKManagerCallback will be used to listen to
+         * the SDK Registration result and the product changing.
+         */
+        mDJISDKManagerCallback = new DJISDKManager.SDKManagerCallback() {
 
+            //Listens to the SDK registration result
             @Override
-            public void onConnectivityChange(boolean isConnected) {
-                notifyStatusChange();
+            public void onRegister(DJIError error) {
+
+                if(error == DJISDKError.REGISTRATION_SUCCESS) {
+
+                    Handler handler = new Handler(Looper.getMainLooper());
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(getApplicationContext(), "Register Success", Toast.LENGTH_LONG).show();
+                        }
+                    });
+
+                    DJISDKManager.getInstance().startConnectionToProduct();
+
+                } else {
+
+                    Handler handler = new Handler(Looper.getMainLooper());
+                    handler.post(new Runnable() {
+
+                        @Override
+                        public void run() {
+                            Toast.makeText(getApplicationContext(), "Register sdk fails, check network is available", Toast.LENGTH_LONG).show();
+                        }
+                    });
+
+                }
+                Log.e("TAG", error.toString());
             }
 
-        };
+            //Listens to the connected product changing, including two parts, component changing or product connection changing.
+            @Override
+            public void onProductChange(BaseProduct oldProduct, BaseProduct newProduct) {
 
-        private void notifyStatusChange() {
-            mHandler.removeCallbacks(updateRunnable);
-            mHandler.postDelayed(updateRunnable, 500);
+                mProduct = newProduct;
+                if(mProduct != null) {
+                    mProduct.setBaseProductListener(mDJIBaseProductListener);
+                }
+
+                notifyStatusChange();
+            }
+        };
+        //Check the permissions before registering the application for android system 6.0 above.
+        int permissionCheck = ContextCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        int permissionCheck2 = ContextCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.READ_PHONE_STATE);
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M || (permissionCheck == 0 && permissionCheck2 == 0)) {
+            //This is used to start SDK services and initiate SDK.
+            DJISDKManager.getInstance().registerApp(getApplicationContext(), mDJISDKManagerCallback);
+            Toast.makeText(getApplicationContext(), "registering, pls wait...", Toast.LENGTH_LONG).show();
+
+        } else {
+            Toast.makeText(getApplicationContext(), "Please check if the permission is granted.", Toast.LENGTH_LONG).show();
         }
+    }
 
-        private Runnable updateRunnable = new Runnable() {
+    private void notifyStatusChange() {
+        mHandler.removeCallbacks(updateRunnable);
+        mHandler.postDelayed(updateRunnable, 500);
+    }
 
-            @Override
-            public void run() {
-                Intent intent = new Intent(FLAG_CONNECTION_CHANGE);
-                sendBroadcast(intent);
-            }
-        };
+    private Runnable updateRunnable = new Runnable() {
+
+        @Override
+        public void run() {
+            Intent intent = new Intent(FLAG_CONNECTION_CHANGE);
+            getApplicationContext().sendBroadcast(intent);
+        }
     };
 
 }
